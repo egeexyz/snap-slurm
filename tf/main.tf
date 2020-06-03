2c251ed350ca7b0b5ac293ff7b167c1455326f09
egee-irl <brian@egee.io>
2020-06-02 17:15:48 -0700

    added tf

---
+++ tf/main.tf
@@ -0,0 +1,21 @@
+variable "api_key" {
+  type = string
+}
+
+provider "vultr" {
+  api_key = var.api_key
+  rate_limit = 700
+  retry_limit = 3
+}
+
+resource "vultr_server" "ubuntu_20_04" {
+    plan_id = "201"
+    region_id = "4"
+    os_id = "387" // TODO: put this in an array for programatic creation
+    script_id = "724476" // This is where the magic happens
+    ssh_key_ids = ["5ec4513d44b0f"]
+}
+
+output "ip_address" {
+  value = vultr_server.ubuntu_20_04.main_ip
+}
