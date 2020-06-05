9a6fa21acc644de7f68fd7b8ea3e61a42d9930c1
egee-irl <brian@egee.io>
2020-06-04 17:59:00 -0700

    use scripts instead of inline

--- tf/main.tf
+++ tf/main.tf
@@ -6,34 +6,12 @@ locals {
   plan_id = "400"
   region_id = "1"
   ssh_key_ids = ["5ec4513d44b0f"]
-  priv_key_path = "/tmp/id_rsa"
+  priv_key_path = "/home/egee/.ssh/id_rsa"#"/tmp/id_rsa"
   filename = "slurm_20.02.1_amd64.snap"
+  ubuntu = ["270", "387"]
   ubuntu_2004 = "387"
   ubuntu_1804 = "270"
   centos8 = "362"
-  ubuntu_cmds = [
-      "snap install --dangerous /tmp/slurm_20.02.1_amd64.snap",
-      "snap connect slurm:network-control",
-      "snap connect slurm:system-observe",
-      "snap connect slurm:hardware-observe",
-      "set slurm snap.mode=all",
-      "systemctl status snap.slurm.* --no-pager",
-      "/var/lib/snapd/snap/bin/slurm.sinfo"
-    ]
-  centos8_cmds = [
-      "dnf install -y epel-release",
-      "dnf upgrade -y",
-      "dnf install -y snapd",
-      "systemctl enable --now snapd.socket",
-      "ln -s /var/lib/snapd/snap /snap",
-      "snap install --dangerous /tmp/slurm_20.02.1_amd64.snap",
-      "snap connect slurm:network-control",
-      "snap connect slurm:system-observe",
-      "snap connect slurm:hardware-observe",
-      "snap set slurm snap.mode=all",
-      "systemctl status snap.slurm.* --no-pager",
-      "/var/lib/snapd/snap/bin/slurm.sinfo"
-    ]
 }
 
 
@@ -43,16 +21,17 @@ provider "vultr" {
   retry_limit = 3
 }
 
-resource "vultr_server" "ubuntu_20_04" {
-    plan_id = local.plan_id
-    region_id = local.region_id
-    os_id = local.ubuntu_2004
-    ssh_key_ids = local.ssh_key_ids
+resource "vultr_server" "ubuntu" {
+  count = length(local.ubuntu)
+  plan_id = local.plan_id
+  region_id = local.region_id
+  os_id = element(local.ubuntu, count.index)
+  ssh_key_ids = local.ssh_key_ids
   provisioner "file" {
     connection {
       type     = "ssh"
       user     = "root"
-      host     = vultr_server.ubuntu_20_04.main_ip
+      host     = self.main_ip
       private_key = file(local.priv_key_path)
     }
     source      = local.filename
@@ -62,61 +41,61 @@ resource "vultr_server" "ubuntu_20_04" {
     connection {
       type     = "ssh"
       user     = "root"
-      host     = vultr_server.ubuntu_20_04.main_ip
+      host     = self.main_ip
       private_key = file(local.priv_key_path)
     }
-    inline = local.ubuntu_cmds
+    script = "scripts/slurm-test.sh"
   }
 }
 
-resource "vultr_server" "ubuntu_18_04" {
-    plan_id = local.plan_id
-    region_id = local.region_id
-    os_id = local.ubuntu_1804
-    ssh_key_ids = local.ssh_key_ids
-  provisioner "file" {
-    connection {
-      type     = "ssh"
-      user     = "root"
-      host     = vultr_server.ubuntu_18_04.main_ip
-      private_key = file(local.priv_key_path)
-    }
-    source      = local.filename
-    destination = "/tmp/${local.filename}"
-  }
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "root"
-      host     = vultr_server.ubuntu_18_04.main_ip
-      private_key = file(local.priv_key_path)
-    }
-    inline = local.ubuntu_cmds
-  }
-}
+# resource "vultr_server" "ubuntu_18_04" {
+#     plan_id = local.plan_id
+#     region_id = local.region_id
+#     os_id = local.ubuntu_1804
+#     ssh_key_ids = local.ssh_key_ids
+#   provisioner "file" {
+#     connection {
+#       type     = "ssh"
+#       user     = "root"
+#       host     = self.ubuntu_18_04.main_ip
+#       private_key = file(local.priv_key_path)
+#     }
+#     source      = local.filename
+#     destination = "/tmp/${local.filename}"
+#   }
+#   provisioner "remote-exec" {
+#     connection {
+#       type     = "ssh"
+#       user     = "root"
+#       host     = self.ubuntu_18_04.main_ip
+#       private_key = file(local.priv_key_path)
+#     }
+#     inline = local.ubuntu_cmds
+#   }
+# }
 
-resource "vultr_server" "centos8" {
-    plan_id = local.plan_id
-    region_id = local.region_id
-    os_id = local.centos8
-    ssh_key_ids = local.ssh_key_ids
-  provisioner "file" {
-    connection {
-      type     = "ssh"
-      user     = "root"
-      host     = vultr_server.centos8.main_ip
-      private_key = file(local.priv_key_path)
-    }
-    source      = local.filename
-    destination = "/tmp/${local.filename}"
-  }
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "root"
-      host     = vultr_server.centos8.main_ip
-      private_key = file(local.priv_key_path)
-    }
-    inline = local.centos8_cmds
-  }
-}
+# resource "vultr_server" "centos8" {
+#     plan_id = local.plan_id
+#     region_id = local.region_id
+#     os_id = local.centos8
+#     ssh_key_ids = local.ssh_key_ids
+#   provisioner "file" {
+#     connection {
+#       type     = "ssh"
+#       user     = "root"
+#       host     = self.centos8.main_ip
+#       private_key = file(local.priv_key_path)
+#     }
+#     source      = local.filename
+#     destination = "/tmp/${local.filename}"
+#   }
+#   provisioner "remote-exec" {
+#     connection {
+#       type     = "ssh"
+#       user     = "root"
+#       host     = self.centos8.main_ip
+#       private_key = file(local.priv_key_path)
+#     }
+#     inline = local.centos8_cmds
+#   }
+# }
