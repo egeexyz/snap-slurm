e1e1483fa5c00b597b250b13ac1fff390ffcc80d
egee-irl <brian@egee.io>
2020-06-08 11:48:21 -0700

    moved into modules

---
+++ tests/ubuntu2004/main.tf
@@ -0,0 +1,39 @@
+variable "api_key" {
+  type = string
+}
+
+module "test_data" {
+  source = "../../tests"
+}
+
+provider "vultr" {
+  api_key = var.api_key
+  rate_limit = 700
+  retry_limit = 3
+}
+
+resource "vultr_server" "ubuntu2004" {
+  plan_id = module.test_data.plan_id
+  region_id = module.test_data.region_id
+  os_id = module.test_data.ubuntu_2004
+  ssh_key_ids = module.test_data.ssh_key_ids
+  provisioner "file" {
+    connection {
+      type     = "ssh"
+      user     = "root"
+      host     = self.main_ip
+      private_key = file(module.test_data.priv_key_path)
+    }
+    source      = "../${module.test_data.filename}"
+    destination = "/tmp/${module.test_data.filename}"
+  }
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "root"
+      host     = self.main_ip
+      private_key = file(module.test_data.priv_key_path)
+    }
+    script = "../slurm-test.sh"
+  }
+}
--- tf/scripts/centos8.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-dnf install -y epel-release
-dnf upgrade -y
-dnf install -y snapd
-systemctl enable --now snapd.socket
-ln -s /var/lib/snapd/snap /snap
