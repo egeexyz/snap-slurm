b75bdb592c3fd5adf15c189efe8255c07c361ba3
egee-irl <brian@egee.io>
2020-06-16 11:15:46 -0700

    run test

--- tests/slurm-test.sh
+++ tests/slurm-test.sh
@@ -34,16 +34,16 @@ slurm.version
 # TODO: Enable tests below
 
 # TODO: The || is needed because the first hit always fails
-# slurm.sinfo || slurm.sinfo
-# slurm.sacct
-# slurm.sdiag
-# slurm.sprio
-# slurm.sattach
-# slurm.squeue
-# slurm.srun -p debug -n 1 hostname
+slurm.sinfo || slurm.sinfo
+slurm.sacct
+slurm.sdiag
+slurm.sprio
+slurm.sattach
+slurm.squeue
+slurm.srun -p debug -n 1 hostname
 # slurm.srun --uid 1000 -N1 -l hostname
-# scontrol show config
-# slurm.sshare
+scontrol show config
+slurm.sshare
 
 ## TODO: Need to test the following commands
 
