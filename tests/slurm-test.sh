542cfc60f9e76179e1243cdb31efa7511c421acc
egee-irl <brian@egee.io>
2020-06-16 14:42:04 -0700

    add user, sleep after mode=all

--- tests/slurm-test.sh
+++ tests/slurm-test.sh
@@ -24,13 +24,13 @@ snap connect slurm:system-observe   || echo 'Plug not supported, passing...'
 snap connect slurm:hardware-observe || echo 'Plug not supported, passing...'
 
 echo 'Beginning Test Suite'
-systemctl restart snap.slurm.slurmctld
-sleep 10
 snap set slurm snap.mode=all
+sleep 10
 snap services
 
-slurm.version
+adduser --disabled-password --gecos "" ubuntu
 
+slurm.version
 slurm.sinfo || slurm.sinfo
 
 echo 'Testing: slurm.srun -p debug -n 1 hostname' && slurm.srun -p debug -n 1 hostname
@@ -40,11 +40,12 @@ echo 'Testing: slurm.squeue'   && slurm.squeue
 echo 'Testing: slurm.sshare'   && slurm.sshare
 echo 'Testing: slurm.sacct'    && slurm.sacct
 echo 'Testing: slurm.sdiag'    && slurm.sdiag
-echo 'Testing: slurm.sprio'    && slurm.sprio
-# echo 'Testing: slurm.sattach'  && slurm.sattach
+echo 'Testing: slurm.sattach'  && slurm.sattach
 
 ## TODO: Need to test the following commands
 # slurm.srun --uid 1000 -N1 -l hostname
+
+# echo 'Testing: slurm.sprio'    && slurm.sprio # Requires priority/multifactor plugin
 
 # slurm.sattach
 # slurm.sacctmgr
